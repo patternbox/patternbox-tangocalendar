7b5d4338253fdae8c5e5f23706e8da655a99fcb7
patternbox <ehms@patternbox.com>
2014-02-03 19:54:57 +0100

    DDD ValueObject interface

---
+++ tangocal-core/src/main/java/com/patternbox/tangocalendar/types/ValueObject.java
@@ -0,0 +1,21 @@
+package com.patternbox.tangocalendar.types;
+
+import java.io.Serializable;
+
+/**
+ * A value object, as described in the DDD book.
+ * 
+ * @author <a href='http://www.patternbox.com'>D. Ehms, Patternbox</a>
+ */
+public interface ValueObject<T> extends Serializable {
+
+	/**
+	 * Value objects compare by the values of their attributes, they don't have an identity.
+	 * 
+	 * @param other
+	 *          The other value object.
+	 * @return <code>true</code> if the given value object's and this value object's attributes are
+	 *         the same.
+	 */
+	boolean sameValueAs(T other);
+}
