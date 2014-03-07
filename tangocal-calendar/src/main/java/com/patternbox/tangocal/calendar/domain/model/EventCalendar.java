3d1c7cdd5fe5398a99f95e5702e1dbbb22bda7b0
patternbox <ehms@patternbox.com>
2014-03-07 09:59:13 +0100

    Package name update

--- tangocal-calendar/src/main/java/com/patternbox/tangocal/calendar/domain/model/EventCalendar.java
+++ tangocal-calendar/src/main/java/com/patternbox/tangocal/calendar/domain/model/EventCalendar.java
@@ -23,7 +23,7 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGE.
  ******************************************************************************/
-package com.patternbox.tangocal.calendar.domain;
+package com.patternbox.tangocal.calendar.domain.model;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
