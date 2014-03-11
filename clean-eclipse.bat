5d69e2df7851a9bdad37e584b147338c44076699
patternbox <ehms@patternbox.com>
2014-03-11 10:34:28 +0100

    Development environment housekeeping

---
+++ clean-eclipse.bat
@@ -0,0 +1,3 @@
+rem call mvn eclipse:clean
+
+rd /S /Q .settings 
---
+++ setup-jboss.bat
@@ -0,0 +1,4 @@
+
+set JBOSS_HOME=D:\java.server\jboss-as-7.1.1.Final
+
+%JBOSS_HOME%\bin\jboss-cli.sh --connect --file=setup-jboss.cli
